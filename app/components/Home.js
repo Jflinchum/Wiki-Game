dbe840b36886f7cb4ae8bcf1c22ac781146209bd
Jflinchum <jonflinchum@gmail.com>
2017-02-14 12:52:38 -0500

    Added react bootstrap inputs for options. Imported react bootstrap and fontawesome. Added options to set min and max par. Page loads are tracked now as a 'click counter'.

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
+import { Form, Col, Row, FormControl, ControlLabel, Button } from 'react-bootstrap';
+import classNames from 'classnames';
 import styles from './Home.css';
 import { generateRandom } from '../utils/generate';
 
@@ -9,17 +11,26 @@ export default class Home extends Component {
     super(props);
     this.state = {
       history: [],
-      par: 0,
+      minPar: 3,
+      maxPar: 20,
       body: null,
-      clicks: 0
+      clicks: null,
+      initArt: '',
+      goalArt: '',
     };
+    this.refresh = this.refresh.bind(this);
   }
 
   componentDidMount() {
-    const par = Math.floor((Math.random() * (18)) + 3);
+    this.refresh();
+  }
+
+  refresh() {
+    const par = Math.floor((Math.random() *
+    (this.state.maxPar - this.state.minPar)) + this.state.minPar);
     generateRandom(par, (history) => {
       console.log(history);
-      this.setState({ history, par });
+      this.setState({ history, clicks: null });
     });
   }
 
@@ -27,15 +38,53 @@ export default class Home extends Component {
     return (
       <div>
         <div className={styles.container}>
-          {
-            this.state.history.length > 0
-            ? <h4>Get from {` ${this.state.history[0].title} `}
-              to {` ${this.state.history[this.state.history.length - 1].title} `} with a
-              par of {` ${this.state.par}.`} Current clicks: {` ${this.state.clicks - 1}.`}</h4>
-            : null
-          }
+          <Row className={classNames(styles.row)}>
+            {
+              this.state.history.length > 0
+              ? <h4>Get from {` ${this.state.history[0].title} `}
+                to {` ${this.state.history[this.state.history.length - 1].title} `} with a
+                par of {` ${this.state.history.length - 1}.`} Current clicks: {` ${this.state.clicks ? this.state.clicks : 0}.`}</h4>
+              : null
+            }
+          </Row>
+          <Form inline>
+            <Row className={classNames(styles.row, styles.options)}>
+              <Col componentClass={ControlLabel} xs={2}>
+                Initial Article
+              </Col>
+              <Col xs={3}>
+                <FormControl type="text" placeholder={this.state.history[0] ? this.state.history[0].link : ''} onChange={(event) => { this.setState({ initArt: event.target.value }); }} />
+              </Col>
+              <Col componentClass={ControlLabel} xs={2}>
+                Minimum Par
+              </Col>
+              <Col xs={3}>
+                <FormControl type="text" placeholder={this.state.minPar ? this.state.minPar : ''} onChange={(event) => { this.setState({ minPar: event.target.value }); }} />
+              </Col>
+              <Col componentClass={ControlLabel} xs={2}>
+                Maximum Par
+              </Col>
+              <Col xs={3}>
+                <FormControl type="text" placeholder={this.state.maxPar ? this.state.maxPar : ''} onChange={(event) => { this.setState({ maxPar: event.target.value }); }} />
+              </Col>
+              <Col xs={2}>
+                <Button bsStyle="default" onClick={() => { this.refresh(); }}>Refresh</Button>
+              </Col>
+            </Row>
+          </Form>
           <div>
-            {this.state.history[0] ? <object type="text/html" data={this.state.history[0].link} width="100%" height="600px" style={{ overflow: 'auto', border: '5px', ridge: 'blue' }} onLoad={() => { this.setState({ clicks: this.state.clicks + 1 }); }} /> : null}
+            {this.state.history[0] ?
+              <object
+                type="text/html" data={this.state.history[0].link} width="100%" height="600px"
+                onLoad={() => {
+                  if (this.state.clicks !== null) {
+                    this.setState({ clicks: this.state.clicks + 1 });
+                  } else {
+                    this.setState({ clicks: 0 });
+                  }
+                }}
+              />
+              : null}
           </div>
         </div>
       </div>
