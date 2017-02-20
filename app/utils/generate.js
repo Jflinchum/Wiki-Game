5c304a396bb40c74d9c62f3e4d2e6bab1d9133b9
Jflinchum <jonflinchum@gmail.com>
2017-02-19 20:45:25 -0500

    Added BFS of two wiki articles to find the path between them.

--- app/utils/generate.js
+++ app/utils/generate.js
@@ -19,11 +19,110 @@ const getLinks = (body) => {
   return matches;
 };
 
-// request('https://en.wikipedia.org/wiki/Clivina_okutanii', (error, response, body) => {
-//   console.log(getLinks(body));
+// const getPath = (url, goalUrl, cb) => {
+//   let searchDone = false;
+//   const findPath = (url1, url2, path, callback) => {
+//     if (searchDone) {
+//       return;
+//     }
+//     path.push(url1);
+//     if (url1 === goalUrl) {
+//       return callback(null, path);
+//     }
+//     request(url1, (error, response, body) => {
+//       if (!error && response.statusCode === 200) {
+//         const links = getLinks(body);
+//         links.forEach((link) => { findPath(link, goalUrl, path, callback); });
+//       } else {
+//         return callback(new Error('Error getting wiki page.'));
+//       }
+//     });
+//   };
+//   findPath(url, goalUrl, [], (error, path) => {
+//     searchDone = true;
+//     return cb(error, path);
+//   });
+// };
+
+class Path {
+  constructor(path) {
+    this.path = path;
+  }
+
+  async generate() {
+    return new Promise((resolve, reject) => {
+      request(this.path[this.path.length - 1], (error, response, body) => {
+        if (!error && response.statusCode === 200) {
+          const links = getLinks(body);
+          const newPaths = [];
+          links.forEach((link) => {
+            if (!this.path.includes(link)) {
+              newPaths.push(new Path([...this.path, link]));
+            }
+          });
+          return resolve(newPaths);
+        }
+        return reject(new Error('Error getting wiki page.'));
+      });
+    });
+  }
+}
+
+const turnPathToLink = (path) => {
+  return new Promise((resolve, reject) => {
+    request(path, (error, response, body) => {
+      if (error) {
+        return reject(error);
+      }
+      return resolve({ title: getTitle(body), link: path });
+    });
+  });
+};
+
+const getPath = async (url1, url2, cb) => {
+  const paths = [];
+  paths.push(new Path([url1]));
+  try {
+    while (true) {
+      let potentialPath =
+      paths.filter((finalPath) => (finalPath.path.includes(url2)));
+      if (potentialPath.length > 0) {
+        potentialPath = potentialPath[0].path;
+        const finalPath = [];
+        potentialPath.map(async (path) => {
+          try {
+            finalPath.push(turnPathToLink(path));
+          } catch (e) {
+            return cb(e);
+          }
+        });
+        return cb(null, await Promise.all(finalPath));
+      }
+      const generateFunctions = [];
+      while (paths.length > 0) {
+        generateFunctions.push(paths.pop().generate());
+      }
+      const results = await Promise.all(generateFunctions);
+      results.forEach((path) => {
+        paths.push(...path);
+      });
+    }
+  } catch (e) {
+    return cb(e);
+  }
+};
+
+// getPath(
+// 'https://en.wikipedia.org/wiki/Overwatch_(video_game)',
+// 'https://en.wikipedia.org/wiki/Undertale',
+// (error, path) => {
+//   if (error) {
+//     console.log(error);
+//   }
+//   console.log(path, 'final path');
 // });
 
-const getPath = (url, distance, history, cb) => {
+const getRandomPath = (url, distance, history, cb) => {
   request(url, (error, response, body) => {
     if (!error && response.statusCode === 200) {
       const tempHistory = history;
@@ -35,18 +134,18 @@ const getPath = (url, distance, history, cb) => {
         if (distance <= 0) {
           return cb(null, history);
         }
-        return getPath(nextLink, distance - 1, history, cb);
+        return getRandomPath(nextLink, distance - 1, history, cb);
       } catch (e) {
         console.log(e);
         console.log(history);
-        return getPath(
+        return getRandomPath(
           tempHistory[tempHistory.length - 2],
           distance + 1,
           tempHistory.slice(0, tempHistory.length - 1),
           cb);
       }
     } else if (history.length === 0) {
-      return getPath(
+      return getRandomPath(
         history[history.length - 2],
         distance + 1,
         history.slice(0, history.length - 1),
@@ -61,19 +160,21 @@ const generate = ({ initArticle, goalArticle, par }, cb) => {
   if (initArticle && !goalArticle) {
     const urlRegex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)/g;
     if (initArticle.match(urlRegex)) { // If the initArticle is a url
-      getPath(initArticle, par, [], cb);
+      getRandomPath(initArticle, par, [], cb);
     } else { // If it isn't a url
       return cb(new Error('Invalid wiki url'));
     }
   } else if (goalArticle && !initArticle) {
-    getPath(goalArticle, par, [], (error, history) => {
+    getRandomPath(goalArticle, par, [], (error, history) => {
       if (error) {
         return cb(error);
       }
       return cb(null, history.reverse());
     });
+  } else if (initArticle && goalArticle) {
+    getPath(initArticle, goalArticle, cb);
   } else {
-    getPath('https://en.wikipedia.org/wiki/Special:Random', par, [], cb);
+    getRandomPath('https://en.wikipedia.org/wiki/Special:Random', par, [], cb);
   }
 };
 
@@ -83,5 +184,3 @@ const generate = ({ initArticle, goalArticle, par }, cb) => {
 export default {
   generate
 };
-
-// generateRandom(15, (history) => { console.log(history); });
